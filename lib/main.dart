2597359fa870e1d95ff4633ef02e4c728bf30d4b
LOLwierd <ayaan.180410107098@gmail.com>
2020-05-18 15:42:14 +0530

    Fixed some errors Bad code still

--- lib/main.dart
+++ lib/main.dart
@@ -28,15 +28,15 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  String ans;
-  num pre;
-  num post;
-  String operator="";
+  var ans="";
+  var pre;
+  var post="";
+  var operator;
   void calculate(str){
     setState(() {
-      if(pre==null && str is num){
+      if(pre=="" && str is num){
         ans=str.toString();
-        pre=str;
+        pre=str.toString();
         log("str:"+str.toString());
         log("pre:"+pre.toString());
         log("post:"+post.toString());
@@ -44,9 +44,25 @@ class _MyHomePageState extends State<MyHomePage> {
         return;
       }
       else if(str is num){
-        ans = str.toString();
-//        pre=null;
-        post=str;
+        if(str!=0 && pre!="0"){
+          ans += str.toString();
+        }
+        else if(pre=="0") {
+          ans = str.toString();
+          pre=str.toString();
+          post="";
+          return;
+        }
+        if(operator==null) {
+          pre += str.toString();
+          ans=pre;
+        }
+        else {
+          if(post=="0")
+            post=str.toString();
+          else
+            post += str.toString();
+        }
         log("in else if");
         log("str:"+str.toString());
         log("pre:"+pre.toString());
@@ -56,47 +72,48 @@ class _MyHomePageState extends State<MyHomePage> {
       }
       switch(str){
         case("AC"):
-          ans=null;
-          pre=null;
-          post=null;
+          ans="";
+          pre="";
+          post="";
+          operator=null;
           log("AC");
           break;
         case("C"):
           ans="0";
           break;
         case("+"):
-          ans="";
+          ans+="+";
           operator="+";
           break;
         case("-"):
-          ans="";
+          ans+="-";
           operator="-";
           break;
         case("*"):
-          ans="";
+          ans+="*";
           operator="*";
           break;
         case("/"):
-          ans="";
+          ans+="/";
           operator="/";
           break;
         case("="):
-          if(operator!=null) {
+          if(operator!="") {
             switch (operator) {
               case("+"):
-                pre = pre + post;
+                pre = (double.parse(pre) + double.parse(post)).toString();
                 operator=null;
                 break;
               case("-"):
-                pre = post - pre;
+                pre = (double.parse(pre) - double.parse(post)).toString();
                 operator=null;
                 break;
               case("*"):
-                pre = pre * post;
+                pre = (double.parse(pre) * double.parse(post)).toString();
                 operator=null;
                 break;
               case("/"):
-                pre = post / pre;
+                pre = (double.parse(pre) / double.parse(post)).toString();
                 operator=null;
                 break;
             }
@@ -105,9 +122,9 @@ class _MyHomePageState extends State<MyHomePage> {
             log("No Operator");
           }
           log("IN equals");
-          log("pre:"+pre.toString());
-          log("post:"+post.toString());
-          ans=pre.toString();
+          log("pre:"+pre);
+          log("post:"+post);
+          ans=pre;
           break;
       }
 
@@ -121,198 +138,200 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text(
-                  "$ans"
+        child: Container(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                    "$ans"
+                ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(4.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.all(4.0),
-                    child: FlatButton(
-                      onPressed: ()=> {calculate(9)},
-                      child:Text(
-                        "9",
-                    ),
-                  ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(4.0),
-                    child: FlatButton(
-                      onPressed: ()=> {calculate(8)},
-                      child:Text(
-                        "8",
+              Padding(
+                padding: const EdgeInsets.all(4.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.all(4.0),
+                      child: FlatButton(
+                        onPressed: ()=> {calculate(9)},
+                        child:Text(
+                          "9",
                       ),
                     ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(4.0),
-                    child: FlatButton(
-                      onPressed: ()=> {calculate(7)},
-                      child:Text(
-                        "7",
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(4.0),
+                      child: FlatButton(
+                        onPressed: ()=> {calculate(8)},
+                        child:Text(
+                          "8",
+                        ),
                       ),
                     ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(4.0),
-                    child: FlatButton(
-                      onPressed: ()=> {calculate("+")},
-                      child:Text(
-                        "+",
+                    Padding(
+                      padding: const EdgeInsets.all(4.0),
+                      child: FlatButton(
+                        onPressed: ()=> {calculate(7)},
+                        child:Text(
+                          "7",
+                        ),
                       ),
                     ),
-                  ),
-                ],
+                    Padding(
+                      padding: const EdgeInsets.all(4.0),
+                      child: FlatButton(
+                        onPressed: ()=> {calculate("+")},
+                        child:Text(
+                          "+",
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(4.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.all(4.0),
-                    child: FlatButton(
-                      onPressed: ()=> {calculate(6)},
-                      child:Text(
-                        "6",
+              Padding(
+                padding: const EdgeInsets.all(4.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.all(4.0),
+                      child: FlatButton(
+                        onPressed: ()=> {calculate(6)},
+                        child:Text(
+                          "6",
+                        ),
                       ),
                     ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(4.0),
-                    child: FlatButton(
-                      onPressed: ()=> {calculate(5)},
-                      child:Text(
-                        "5",
+                    Padding(
+                      padding: const EdgeInsets.all(4.0),
+                      child: FlatButton(
+                        onPressed: ()=> {calculate(5)},
+                        child:Text(
+                          "5",
+                        ),
                       ),
                     ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(4.0),
-                    child: FlatButton(
-                      onPressed: ()=> {calculate(4)},
-                      child:Text(
-                        "4",
+                    Padding(
+                      padding: const EdgeInsets.all(4.0),
+                      child: FlatButton(
+                        onPressed: ()=> {calculate(4)},
+                        child:Text(
+                          "4",
+                        ),
                       ),
                     ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(4.0),
-                    child: FlatButton(
-                      onPressed: ()=> {calculate("-")},
-                      child:Text(
-                        "-",
+                    Padding(
+                      padding: const EdgeInsets.all(4.0),
+                      child: FlatButton(
+                        onPressed: ()=> {calculate("-")},
+                        child:Text(
+                          "-",
+                        ),
                       ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(4.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.all(4.0),
-                    child: FlatButton(
-                      onPressed: ()=> {calculate(3)},
-                      child:Text(
-                        "3",
+              Padding(
+                padding: const EdgeInsets.all(4.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.all(4.0),
+                      child: FlatButton(
+                        onPressed: ()=> {calculate(3)},
+                        child:Text(
+                          "3",
+                        ),
                       ),
                     ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(4.0),
-                    child: FlatButton(
-                      onPressed: ()=> {calculate(2)},
-                      child:Text(
-                        "2",
+                    Padding(
+                      padding: const EdgeInsets.all(4.0),
+                      child: FlatButton(
+                        onPressed: ()=> {calculate(2)},
+                        child:Text(
+                          "2",
+                        ),
                       ),
                     ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(4.0),
-                    child: FlatButton(
-                      onPressed: ()=> {calculate(1)},
-                      child:Text(
-                        "1",
+                    Padding(
+                      padding: const EdgeInsets.all(4.0),
+                      child: FlatButton(
+                        onPressed: ()=> {calculate(1)},
+                        child:Text(
+                          "1",
+                        ),
                       ),
                     ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(4.0),
-                    child: FlatButton(
-                      onPressed: ()=> {calculate("*")},
-                      child:Text(
-                        "*",
+                    Padding(
+                      padding: const EdgeInsets.all(4.0),
+                      child: FlatButton(
+                        onPressed: ()=> {calculate("*")},
+                        child:Text(
+                          "*",
+                        ),
                       ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(4.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.all(4.0),
-                    child: FlatButton(
-                      onPressed: ()=> {calculate("C")},
-                      child:Text(
-                        "C",
+              Padding(
+                padding: const EdgeInsets.all(4.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.all(4.0),
+                      child: FlatButton(
+                        onPressed: ()=> {calculate("C")},
+                        child:Text(
+                          "C",
+                        ),
                       ),
                     ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(4.0),
-                    child: FlatButton(
-                      onPressed: ()=> {calculate(0)},
-                      child:Text(
-                        "0",
+                    Padding(
+                      padding: const EdgeInsets.all(4.0),
+                      child: FlatButton(
+                        onPressed: ()=> {calculate(0)},
+                        child:Text(
+                          "0",
+                        ),
                       ),
                     ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(4.0),
-                    child: FlatButton(
-                      onPressed: ()=> {calculate("AC")},
-                      child:Text(
-                        "AC",
+                    Padding(
+                      padding: const EdgeInsets.all(4.0),
+                      child: FlatButton(
+                        onPressed: ()=> {calculate("AC")},
+                        child:Text(
+                          "AC",
+                        ),
                       ),
                     ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(4.0),
-                    child: FlatButton(
-                      onPressed: ()=> {calculate("/")},
-                      child:Text(
-                        "/",
+                    Padding(
+                      padding: const EdgeInsets.all(4.0),
+                      child: FlatButton(
+                        onPressed: ()=> {calculate("/")},
+                        child:Text(
+                          "/",
+                        ),
                       ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-            FlatButton(
-              onPressed: ()=>{calculate("=")},
-              child: Text(
-                  "="
-              ),
-            )
-          ],
+              FlatButton(
+                onPressed: ()=>{calculate("=")},
+                child: Text(
+                    "="
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
